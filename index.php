d895a8a089eb04eb6207cd8b6b9b1a7befab4c71
luisangeluis <luisangeluis@gmail.com>
2021-05-01 20:04:55 -0500

    Se crean las plantillas video 13 pendiente

--- index.php
+++ index.php
@@ -2,81 +2,12 @@
   include_once 'app/Conexion.inc.php';
   include_once 'app/CRepositorioUsuarios.inc.php';
   
-  Conexion:: openConexion();
-
-  //$usuarios = CRepositorioUsuarios :: getAllUsers(Conexion::getConexion());
-  $totalUsuarios = CRepositorioUsuarios :: getNumUsers(Conexion::getConexion());
-  echo $totalUsuarios;
-  Conexion:: closeConexion();
+  
+  include_once './plantillas/documento-declaracion.inc.php';
+  include_once './plantillas/navbar.inc.php';
 ?>
-<!doctype html>
-<html lang="es">
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-  <title>Blog 3</title>
-  <link rel="stylesheet" href="./styles/mainStyles.css">
-  <style>
-
-    body {
-      padding-top: 3.5rem;
-    }
-  </style>
-</head>
-
-<body>
-  <nav class="navbar navbar-expand-md  fixed-top bg-secondary">
-    <div class="container">
-      <a class="navbar-brand" href="#">BLOG LUIS-3</a>
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="visually-hidden">Boton para barra de navegacion</span>
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" >Disabled</a>
-          </li>
-        </ul>
-
-        <ul class="navbar-nav ml-auto mb-2 mb-md-0 ">
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">
-            Usuarios Registrados
-            <?php
-              echo $totalUsuarios;
-            ?>
-            </a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Iniciar Sesion</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Registro</a>
-          </li>
-
-        </ul>
-
-      </div>
-    </div>
-  </nav>
+  
   <!--JUMBOTRON-->
   <div class="container">
     <div class="jumbotron rounded my-3 px-5 ">
@@ -147,7 +78,7 @@
 
       <div class="col-lg-8">
         <div class="card">
-          <div class="card-header"><i class="far fa-sticky-note"></i>
+          <div class="card-header"><i class="fas fa-list"></i>
             Ultimas entradas
           </div>
           <div class="card-body">
@@ -160,21 +91,9 @@
   </div>
 
 
+<?php
+include_once './plantillas/documento-cierre.inc.php';
+?>
 
 
-
-  <!-- Optional JavaScript; choose one of the two! -->
-
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-    crossorigin="anonymous"></script>
-
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    -->
-</body>
-
-</html>
+  
---
+++ plantillas/navbar.inc.php
@@ -0,0 +1,53 @@
+<?php
+    Conexion:: openConexion();
+
+    //$usuarios = CRepositorioUsuarios :: getAllUsers(Conexion::getConexion());
+    $totalUsuarios = CRepositorioUsuarios :: getNumUsers(Conexion::getConexion());
+    echo $totalUsuarios;
+    Conexion:: closeConexion();
+?>
+<nav class="navbar navbar-expand-md  fixed-top bg-secondary">
+    <div class="container">
+      <a class="navbar-brand" href="#">BLOG LUIS-3</a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="visually-hidden">Boton para barra de navegacion</span>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#">Entradas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Favoritos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" >Autores</a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto mb-2 mb-md-0 ">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#">
+            <i class="fas fa-user-friends"></i> <!--Usuarios Registrados-->
+            <?php
+              echo $totalUsuarios;
+            ?>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#"><span><i class="fas fa-sign-in-alt"></i></span>Iniciar Sesion</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-plus"></i>Registro</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
