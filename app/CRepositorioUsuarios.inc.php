739a1d28f9c5b17fc44fd4407d1cf43da3bf8a06
luisangeluis <luisangeluis@gmail.com>
2021-06-10 22:33:33 -0500

    Se crea var para password y se esta creando metodo para insertar usuario video 20 pendiente

--- app/CRepositorioUsuarios.inc.php
+++ app/CRepositorioUsuarios.inc.php
@@ -52,4 +52,26 @@ class CRepositorioUsuarios{
 
         return $totalUsuarios;
     }
+
+    //Insertar usuario
+    public static function InsertarUsuario($pConexion, $pUsuario){
+
+        $usuarioInsertado=false;
+
+        if(isset($pConexion)){
+            try{
+                $sql = 'INSERT INTO usuarios(nombre,email,password,fecha_registro,activo) VALUES(:nombre,:email,:password,NOW(),0)';
+                $sentencia = $pConexion->prepare($sql);
+
+                $sentencia ->bindParam(':nombre',$pUsuario ->getNombre(), PDO::PARAM_STR);
+                $sentencia ->bindParam(':email',$pUsuario ->getEmail(), PDO::PARAM_STR);
+                $sentencia ->bindParam(':password',$pUsuario ->getPassword(), PDO::PARAM_STR);
+
+                $usuarioInsertado = $sentencia->execute();
+            }catch(PDOException $e){
+                print 'ERROR' . $e->getMessage();
+
+            }
+        }
+    }
 }
--- app/validadorRegisto.inc.php
+++ app/validadorRegisto.inc.php
@@ -1,151 +1,180 @@
 <?php
-    class validadorRegistro{
-        //Varibles para crear un aviso en pantalla
-        private $avisoInicio;
-        private $avisoCierre;
+class validadorRegistro
+{
+    //Varibles para crear un aviso en pantalla
+    private $avisoInicio;
+    private $avisoCierre;
 
-        private $nombre;
-        private $email;
+    private $nombre;
+    private $email;
+    private $password;
 
-        private $errorNombre;
-        private $errorEmail;
-        private $errorClave1;
-        private $errorClave2;
+    private $errorNombre;
+    private $errorEmail;
+    private $errorClave1;
+    private $errorClave2;
 
-        public function __construct($pNombre, $pEmail, $pClave1,$pClave2){
+    public function __construct($pNombre, $pEmail, $pClave1, $pClave2)
+    {
 
-            $this->avisoInicio = '<br><div class="alert alert-danger" role="alert">';
-            $this->avisoCierre='</div>';
-            $this->nombre="";
-            $this->email="";
+        $this->avisoInicio = '<br><div class="alert alert-danger" role="alert">';
+        $this->avisoCierre = '</div>';
 
-            $this->errorNombre =$this->validarNombre($pNombre);
-            $this->errorEmail = $this->ValidarEmail($pEmail);
-            $this->errorClave1 = $this->validarClave1($pClave1);
-            $this->errorClave2 = $this->validarClave2($pClave1,$pClave2);
+        $this->nombre = "";
+        $this->email = "";
+        $this->password ="";
+
+        $this->errorNombre = $this->validarNombre($pNombre);
+        $this->errorEmail = $this->ValidarEmail($pEmail);
+        $this->errorClave1 = $this->validarClave1($pClave1);
+        $this->errorClave2 = $this->validarClave2($pClave1, $pClave2);
+
+        if($this->errorClave1==="" && $this->errorClave2===""){
+            $this->password = $pClave1;
         }
-
-        private function variableIniciada($variable){
-            if(isset($variable) && !empty($variable)){
-                return true;
-            }else{
-                return false;
-            }
-        }
-
-        private function validarNombre($pNombre){
-            if(!$this-> variableIniciada($pNombre)){
-                return "Debes escribir un nombre de usuario";
-            }else{
-                $this->nombre=$pNombre;
-            }
-
-            if(strlen($pNombre)<6){
-                return "El nombre debe tener minimo 6 caracteres";
-            }
-            if(strlen($pNombre)>24){
-                return "El nombre no debe ser mas largo de 24 caracteres";
-            }
-
-            return "";
-        }
-
-        private function ValidarEmail($pEmail){
-            if(!$this->variableIniciada($pEmail)){
-                return "Debes escribir un email de usuario es panta";
-            }else{
-                $this->email = $pEmail;
-            }
-
-            return "";
-        }
-
-        private function validarClave1($pClave1){
-            if(!$this ->variableIniciada($pClave1)){
-                return "debes escribir una clave";
-            }
-            return "";
-        }
-
-        private function validarClave2($pClave1,$pClave2){
-
-            if(!$this->variableIniciada($pClave1)){
-                return "debes rellar primero la clave 1";
-            }           
-            if($pClave1!==$pClave2){
-                return "debes repetir la contraseña correctamente";
-            }
-            if(!$this->variableIniciada($pClave2)){
-                return "Debes repetir la clave";
-            }
-        }
-
-        //getters
-        public function getNombre(){
-            return $this->nombre;
-
-        }
-        public function getEmail(){
-            return $this->email;
-        }
-
-        public function getErrorNombre(){
-            return $this->errorNombre;
-        }
-        public function getErrorEmail(){
-            return $this->errorEmail;
-        }
-        public function getErrorClave1(){
-            return $this->errorClave1;
-        }
-        public function getErrorClave2(){
-            return $this->errorClave2;
-        }
-
-        public function mostrarNombreEnPantalla(){
-            if($this->nombre!==""){
-                echo 'value="' . $this->nombre . '"';
-            }
-        }
-
-        
-
-        public function mostrarEmailEnPantalla(){
-            if($this->email!==""){
-                echo 'value="' . $this->email . '"';
-            }
-        }
-
-
-        public function mostrarErrorNombreEnPantalla(){
-            if($this->errorNombre!==""){
-                echo $this->avisoInicio . $this->errorNombre . $this->avisoCierre;
-            }
-        }
-        public function mostrarErrorEmailEnPantalla(){
-            if($this->errorEmail!==""){
-                echo $this->avisoInicio . $this->errorEmail . $this->avisoCierre;
-            }
-        }
-
-        public function mostrarErrorClave1EnPantalla(){
-            if($this->errorClave1!==""){
-                echo $this->avisoInicio . $this->errorClave1 . $this->avisoCierre;
-            }
-        }
-
-        public function mostrarErrorClave2EnPantalla(){
-            if($this->errorClave2!==""){
-                echo $this->avisoInicio . $this->errorClave2 . $this->avisoCierre;
-            }
-        }
-
-        public function validarFormulario(){
-            if($this->errorNombre=="" && $this->errorEmail=="" && $this->errorClave1=="" && $this->errorClave2==""){
-                    return true;
-                }else{
-                    return false;
-                }
-        }
-
     }
+
+    private function variableIniciada($variable)
+    {
+        if (isset($variable) && !empty($variable)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function validarNombre($pNombre)
+    {
+        if (!$this->variableIniciada($pNombre)) {
+            return "Debes escribir un nombre de usuario";
+        } else {
+            $this->nombre = $pNombre;
+        }
+
+        if (strlen($pNombre) < 6) {
+            return "El nombre debe tener minimo 6 caracteres";
+        }
+        if (strlen($pNombre) > 24) {
+            return "El nombre no debe ser mas largo de 24 caracteres";
+        }
+
+        return "";
+    }
+
+    private function ValidarEmail($pEmail)
+    {
+        if (!$this->variableIniciada($pEmail)) {
+            return "Debes escribir un email de usuario es panta";
+        } else {
+            $this->email = $pEmail;
+        }
+
+        return "";
+    }
+
+    private function validarClave1($pClave1)
+    {
+        if (!$this->variableIniciada($pClave1)) {
+            return "debes escribir una clave";
+        }
+        return "";
+    }
+
+    private function validarClave2($pClave1, $pClave2)
+    {
+
+        if (!$this->variableIniciada($pClave1)) {
+            return "debes rellar primero la clave 1";
+        }
+        if ($pClave1 !== $pClave2) {
+            return "debes repetir la contraseña correctamente";
+        }
+        if (!$this->variableIniciada($pClave2)) {
+            return "Debes repetir la clave";
+        }
+        return "";
+    }
+
+    //getters y setters
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+    public function getErrorNombre()
+    {
+        return $this->errorNombre;
+    }
+    public function getErrorEmail()
+    {
+        return $this->errorEmail;
+    }
+    public function getErrorClave1()
+    {
+        return $this->errorClave1;
+    }
+    public function getErrorClave2()
+    {
+        return $this->errorClave2;
+    }
+    //Lo muestra como atributo value
+    public function mostrarNombreEnPantalla()
+    {
+        if ($this->nombre !== "") {
+            echo 'value="' . $this->nombre . '"';
+        }
+    }
+
+
+    //Lo muestra como atributo value
+    public function mostrarEmailEnPantalla()
+    {
+        if ($this->email !== "") {
+            echo 'value="' . $this->email . '"';
+        }
+    }
+
+    //Lo muestra como mensaje imprimible
+    public function mostrarErrorNombreEnPantalla()
+    {
+        if ($this->errorNombre !== "") {
+            echo $this->avisoInicio . $this->errorNombre . $this->avisoCierre;
+        }
+    }
+    //Lo muestra como mensaje imprimible
+    public function mostrarErrorEmailEnPantalla()
+    {
+        if ($this->errorEmail !== "") {
+            echo $this->avisoInicio . $this->errorEmail . $this->avisoCierre;
+        }
+    }
+    //Lo muestra como mensaje imprimible
+    public function mostrarErrorClave1EnPantalla()
+    {
+        if ($this->errorClave1 !== "") {
+            echo $this->avisoInicio . $this->errorClave1 . $this->avisoCierre;
+        }
+    }
+    //Lo muestra como mensaje imprimible
+    public function mostrarErrorClave2EnPantalla()
+    {
+        if ($this->errorClave2 !== "") {
+            echo $this->avisoInicio . $this->errorClave2 . $this->avisoCierre;
+        }
+    }
+
+    public function validarFormulario()
+    {
+        if ($this->errorNombre === "" && $this->errorEmail === "" && $this->errorClave1 === "" && $this->errorClave2 === "") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
