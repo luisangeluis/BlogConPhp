97aa23284744e034464e6d8564997bee64070b83
luisangeluis <luisangeluis@gmail.com>
2021-10-11 19:09:00 -0500

    Video 36 pendiente parametros en urls amigables
    Minuto 14 pendiente
    Se configuro algunas lineas para obtener las partes de la ruta de mi pagina mas limpia.
    Se estan configurando para que las rutas de la pagina web sean mas sencillas

--- index.php
+++ index.php
@@ -4,22 +4,48 @@
     $ruta = $componentesUrl['path'];
     // echo $ruta;
     // partesRuta divide la ruta en partes separadas por /
+    // En esta linea la posicion 0 esta vacia la posicion 1 es la raiz y posicion 2 esta vacia
     $partesRuta = explode("/",$ruta);
-    foreach($partesRuta as $parte){
-        // echo $parte;
-    }
+    //array_filter deja las partes vacias como null
+    $partesRuta = array_filter($partesRuta);
+    //Crea un nuevo array ya sin los indices vacios del array y empieza a poner los resultados desde la posicion 0
+    $partesRuta = array_slice($partesRuta,0);
 
-    if($partesRuta[2] === 'registro'){
-        include 'vistas/registro.php';
-        // echo 'if de registro';
-    }else if($partesRuta[2]==='login'){
-        include_once 'vistas/login.php';
+    // if($partesRuta[2] === 'registro'){
+    //     include 'vistas/registro.php';
+    //     // echo 'if de registro';
+    // }else if($partesRuta[2]==='login'){
+    //     include_once 'vistas/login.php';
         
-    }else if($partesRuta[1]=='BlogConPhp'){
-        include_once 'vistas/home.php';
-        //  echo 'if de home';
+    // }else if($partesRuta[1]=='BlogConPhp'){
+    //     include_once 'vistas/home.php';
+    //     //  echo 'if de home';
+    // }
+    // else{
+    //     echo '404';
+    // }
+    $rutaElegida = 'vistas/404.php';
+
+    if($partesRuta[0]=='BlogConPhp'){
+        if(count($partesRuta)==1){
+            $rutaElegida = 'vistas/home.php';
+
+        }else if(count($partesRuta)==2){
+            // echo 'tiene 2 partes';
+            switch($partesRuta[1]){
+                case 'login':
+                    $rutaElegida = 'vistas/login.php';
+                break; 
+                case 'logout':
+                    $rutaElegida = 'vistas/logout.php';
+                break;
+                case 'registro':
+                    $rutaElegida ='vistas/registro.php';
+                break;
+            }
+        }
+
     }
-    else{
-        echo '404';
-    }
+    include_once $rutaElegida;
+
 ?>
