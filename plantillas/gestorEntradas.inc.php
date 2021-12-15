6a6eba1122d3a1bc89480a29248fb11947d4226d
luisangeluis <luisangeluis@gmail.com>
2021-12-14 20:28:43 -0600

    Se completa la vista del gestor entradas
    Se completa la vista del gestor entradas, dar formato a la tabla
    posteriormente.
    Video 46 terminado.

--- plantillas/gestorEntradas.inc.php
+++ plantillas/gestorEntradas.inc.php
@@ -1,1 +1,43 @@
-Gestor entradas
+<div class="row">
+    <div class="col-lg-12">
+        <h2>Gestor Entradas</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <table class="table bg-light">
+            <thead>
+                <tr>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">comentarios</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
