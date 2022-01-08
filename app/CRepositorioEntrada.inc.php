cea2b35cf52be3db10711feb608e676fd9feeb29
luisangeluis <luisangeluis@gmail.com>
2022-01-07 18:59:32 -0600

    Transacciones varias operac. en bd forma segura
    Se revisa el tema transacciones para asegurarse de que todas las
    operaciones se completen o si no, regresen a su estado anterior, asi se
    ejecuntan de forma segura.
    Video 53 terminado.

--- app/CRepositorioEntrada.inc.php
+++ app/CRepositorioEntrada.inc.php
@@ -5,7 +5,7 @@ include_once 'app/config.inc.php';
 
 class CRepositorioEntrada
 {
-    public static function insertarEntrada($pConexion,$pEntrada)
+    public static function insertarEntrada($pConexion, $pEntrada)
     {
         $entradaInsertada = false;
 
@@ -20,7 +20,7 @@ class CRepositorioEntrada
                 $TITULO = $pEntrada->getTitulo();
                 $TEXTO = $pEntrada->getTexto();
                 $ACTIVA = $pEntrada->getActiva();
-                
+
                 $sentencia->bindParam(':autor_id', $AUTOR_ID, PDO::PARAM_STR);
                 $sentencia->bindParam(':urrl', $URL, PDO::PARAM_STR);
                 $sentencia->bindParam(':titulo', $TITULO, PDO::PARAM_STR);
@@ -29,63 +29,72 @@ class CRepositorioEntrada
 
 
                 $entradaInsertada = $sentencia->execute();
-
             } catch (PDOException $e) {
-                print 'ERROR: '. $e->getMessage();
+                print 'ERROR: ' . $e->getMessage();
             }
         }
         return $entradaInsertada;
-
     }
 
-    public static function GetEntradasFechaDescendiente($pConexion){
-        $entradas=[];
-        
-        if(isset($pConexion)){
-            try{
-                $sql ='SELECT * FROM entradas ORDER BY fecha DESC LIMIT 5';
-                
+    public static function GetEntradasFechaDescendiente($pConexion)
+    {
+        $entradas = [];
+
+        if (isset($pConexion)) {
+            try {
+                $sql = 'SELECT * FROM entradas ORDER BY fecha DESC LIMIT 5';
+
                 $sentencia = $pConexion->prepare($sql);
 
                 $sentencia->execute();
 
                 $resultado = $sentencia->fetchAll();
-                if(count($resultado)){
-                    foreach($resultado as $fila){
-                        $entradas[] = new CEntrada($fila['id'],$fila['autor_id'],$fila['url'],$fila['titulo'],$fila['texto'],
-                                        $fila['fecha'],$fila['activa']);
-                        
+                if (count($resultado)) {
+                    foreach ($resultado as $fila) {
+                        $entradas[] = new CEntrada(
+                            $fila['id'],
+                            $fila['autor_id'],
+                            $fila['url'],
+                            $fila['titulo'],
+                            $fila['texto'],
+                            $fila['fecha'],
+                            $fila['activa']
+                        );
                     }
-
                 }
-                }catch(PDOException $e){
-                print 'ERROR: '. $e->getMessage();
- 
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
-
         }
         return $entradas;
-
     }
 
-    public static function getEntradaByUrl($pConexion,$pUrl){
+    public static function getEntradaByUrl($pConexion, $pUrl)
+    {
         $entrada = null;
-        if(isset($pConexion)){
-            try{
-                $sql = 'SELECT * FROM entradas WHERE url LIKE :url'; 
+        if (isset($pConexion)) {
+            try {
+                $sql = 'SELECT * FROM entradas WHERE url LIKE :url';
 
                 $sentencia = $pConexion->prepare($sql);
 
                 $sentencia->bindparam(':url', $pUrl, PDO::PARAM_STR);
-                $sentencia -> execute(); 
+                $sentencia->execute();
                 $resultado = $sentencia->fetch();
 
-                if(!empty($resultado)){
-                    $entrada = new CEntrada($resultado['id'],$resultado['autor_id'],$resultado['url'],$resultado['titulo'],
-                        $resultado['texto'],$resultado['fecha'],$resultado['activa']);
+                if (!empty($resultado)) {
+                    $entrada = new CEntrada(
+                        $resultado['id'],
+                        $resultado['autor_id'],
+                        $resultado['url'],
+                        $resultado['titulo'],
+                        $resultado['texto'],
+                        $resultado['fecha'],
+                        $resultado['activa']
+                    );
                 }
-            }catch(PDOException $e){
-                print 'ERROR: '. $e->getMessage();
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
         }
         return $entrada;
@@ -114,116 +123,131 @@ class CRepositorioEntrada
 
     //         }
     //     }
-        
+
     //     return $entradas;
     // }
-    
-    public static function getEntradasAzar($pConexion,$pLimit){
-        $entradas=[];
 
-        if(isset($pConexion)){
-            try{
+    public static function getEntradasAzar($pConexion, $pLimit)
+    {
+        $entradas = [];
+
+        if (isset($pConexion)) {
+            try {
                 include_once './app/CUsuario.inc.php';
 
                 $sql = "SELECT * FROM entradas ORDER BY RAND() LIMIT $pLimit";
 
-                $sentencia = $pConexion -> prepare($sql);
-                $sentencia ->execute();
+                $sentencia = $pConexion->prepare($sql);
+                $sentencia->execute();
 
-                $resultado = $sentencia -> fetchAll();
+                $resultado = $sentencia->fetchAll();
 
-                if(count($resultado)){
-                    foreach($resultado as $entrada){
-                        $entradas[] = new CEntrada($entrada['id'],$entrada['autor_id'],$entrada['url'],$entrada['titulo'],$entrada['texto'],
-                        $entrada['fecha'],$entrada['activa']);
+                if (count($resultado)) {
+                    foreach ($resultado as $entrada) {
+                        $entradas[] = new CEntrada(
+                            $entrada['id'],
+                            $entrada['autor_id'],
+                            $entrada['url'],
+                            $entrada['titulo'],
+                            $entrada['texto'],
+                            $entrada['fecha'],
+                            $entrada['activa']
+                        );
                     }
                 }
-            }catch(PDOException $e){
-                print 'ERROR: '. $e->getMessage();
-
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
         }
         return $entradas;
     }
 
-    public static function getEntradasAzarByAutor($pConexion,$pUsuario,$pLimit){
+    public static function getEntradasAzarByAutor($pConexion, $pUsuario, $pLimit)
+    {
         $entradas = [];
-        if(isset($pConexion)){
-            try{
+        if (isset($pConexion)) {
+            try {
 
-                $idUsuario = $pUsuario -> getId();
+                $idUsuario = $pUsuario->getId();
                 $sql = "SELECT * FROM entradas WHERE autor_id = $idUsuario ORDER BY id ASC LIMIT $pLimit";
                 $sentencia = $pConexion->prepare($sql);
-                $sentencia -> execute();
-                $resultado = $sentencia -> fetchAll();
+                $sentencia->execute();
+                $resultado = $sentencia->fetchAll();
 
-                if(count($resultado)){
-                    foreach($resultado as $entrada){
-                        $entradas[] = new CEntrada($entrada['id'],$entrada['autor_id'],$entrada['url'],$entrada['titulo'],$entrada['texto'],
-                        $entrada['fecha'],$entrada['activa']);
-                    } 
+                if (count($resultado)) {
+                    foreach ($resultado as $entrada) {
+                        $entradas[] = new CEntrada(
+                            $entrada['id'],
+                            $entrada['autor_id'],
+                            $entrada['url'],
+                            $entrada['titulo'],
+                            $entrada['texto'],
+                            $entrada['fecha'],
+                            $entrada['activa']
+                        );
+                    }
                 }
-            }catch(PDOException $e){
-                print 'ERROR: '. $e->getMessage();
-
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
         }
         return $entradas;
     }
 
-    public static function getEntradasActivasUser($pConexion,$pIdUsuario){
+    public static function getEntradasActivasUser($pConexion, $pIdUsuario)
+    {
         $entradasUser = 0;
 
-        if(isset($pConexion)){
-            try{
+        if (isset($pConexion)) {
+            try {
                 $sql = 'SELECT COUNT(*) as totalEntradas FROM entradas where autor_id = :pIdUsuario AND activa = 1';
-                
-                $sentencia = $pConexion -> prepare($sql);
-                $sentencia -> bindParam(':pIdUsuario',$pIdUsuario,PDO::PARAM_STR);
-                $sentencia -> execute();
-                
+
+                $sentencia = $pConexion->prepare($sql);
+                $sentencia->bindParam(':pIdUsuario', $pIdUsuario, PDO::PARAM_STR);
+                $sentencia->execute();
+
                 $resultado = $sentencia->fetch();
 
-                if(!empty($resultado)){
+                if (!empty($resultado)) {
                     $entradasUser = $resultado['totalEntradas'];
                 }
-                
-            }catch(PDOException $e){
-                print 'ERROR: '. $e->getMessage();
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
         }
         return $entradasUser;
     }
 
-    public static function getEntradasInactivasUser($pConexion,$pIdUsuario){
+    public static function getEntradasInactivasUser($pConexion, $pIdUsuario)
+    {
         $entradasUser = 0;
 
-        if(isset($pConexion)){
-            try{
+        if (isset($pConexion)) {
+            try {
                 $sql = 'SELECT COUNT(*) as totalEntradas FROM entradas where autor_id = :pIdUsuario AND activa = 0';
-                
-                $sentencia = $pConexion -> prepare($sql);
-                $sentencia -> bindParam(':pIdUsuario',$pIdUsuario,PDO::PARAM_STR);
-                $sentencia -> execute();
-                
+
+                $sentencia = $pConexion->prepare($sql);
+                $sentencia->bindParam(':pIdUsuario', $pIdUsuario, PDO::PARAM_STR);
+                $sentencia->execute();
+
                 $resultado = $sentencia->fetch();
 
-                if(!empty($resultado)){
+                if (!empty($resultado)) {
                     $entradasUser = $resultado['totalEntradas'];
                 }
-                
-            }catch(PDOException $e){
-                print 'ERROR: '. $e->getMessage();
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
         }
         return $entradasUser;
     }
-    
-    public static function getEntradasUserFechaDescendente($pConexion,$idUsuario){
+
+    public static function getEntradasUserFechaDescendente($pConexion, $idUsuario)
+    {
         $entradas = [];
-        
-        if(isset($pConexion)){
-            try{
+
+        if (isset($pConexion)) {
+            try {
                 $sql = 'SELECT e.id,e.autor_id,e.url,e.titulo,e.texto,e.fecha,e.activa, COUNT(c.id) as totalComentarios
                 FROM entradas AS e
                 LEFT JOIN comentarios AS c 
@@ -232,87 +256,115 @@ class CRepositorioEntrada
                 GROUP BY e.id
                 ORDER BY e.fecha DESC';
 
-                $sentencia = $pConexion -> prepare($sql);
-                $sentencia -> bindParam(':idUsuario',$idUsuario,PDO::PARAM_STR);
-                $sentencia -> execute();
+                $sentencia = $pConexion->prepare($sql);
+                $sentencia->bindParam(':idUsuario', $idUsuario, PDO::PARAM_STR);
+                $sentencia->execute();
 
-                $resultado = $sentencia -> fetchAll();
+                $resultado = $sentencia->fetchAll();
 
-                if(count($resultado)){
-                    foreach($resultado as $fila){
-                        $entradas[]= array(
-                            new CEntrada($fila['id'],$fila['autor_id'],$fila['url'],$fila['titulo'],$fila['texto'],$fila['fecha'],$fila['activa']
+                if (count($resultado)) {
+                    foreach ($resultado as $fila) {
+                        $entradas[] = array(
+                            new CEntrada(
+                                $fila['id'],
+                                $fila['autor_id'],
+                                $fila['url'],
+                                $fila['titulo'],
+                                $fila['texto'],
+                                $fila['fecha'],
+                                $fila['activa']
                             ),
                             $fila['totalComentarios']
                         );
                     }
                 }
-
-            }catch(PDOException $e){
-                print 'ERROR: '.$e->getMessage();
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
             }
         }
 
         return $entradas;
     }
 
-    public static function tituloExiste($pConexion,$pTitulo){
-        
+    public static function tituloExiste($pConexion, $pTitulo)
+    {
+
         $tituloExiste = null;
-        if(isset($pConexion)){
+        if (isset($pConexion)) {
 
-            try{
+            try {
                 $sql = 'SELECT * FROM entradas WHERE titulo = :pTitulo';
-                
-                $sentencia = $pConexion -> prepare($sql);
-                $sentencia -> bindParam(':pTitulo',$pTitulo,PDO::PARAM_STR);
-                $sentencia ->execute();
 
-                $resultado = $sentencia -> fetchAll();
+                $sentencia = $pConexion->prepare($sql);
+                $sentencia->bindParam(':pTitulo', $pTitulo, PDO::PARAM_STR);
+                $sentencia->execute();
 
-                if(count($resultado)){
+                $resultado = $sentencia->fetchAll();
+
+                if (count($resultado)) {
                     $tituloExiste = true;
-                    
-                }
-                else{
+                } else {
                     $tituloExiste = false;
                 }
-
-            }catch(PDOException $e){
-                print "ERROR". $e->getMessage();
+            } catch (PDOException $e) {
+                print "ERROR" . $e->getMessage();
             }
-            
-        }   
+        }
         return $tituloExiste;
     }
 
-    public static function URLExiste($pConexion,$pUrl){
+    public static function URLExiste($pConexion, $pUrl)
+    {
         $urlExiste = null;
 
-        if(isset($pConexion)){
-            try{
-                $url = "SELECT COUNT(*) AS total FROM entradas WHERE url = :pUrl";
+        if (isset($pConexion)) {
+            try {
+                $sql = "SELECT COUNT(*) AS total FROM entradas WHERE url = :pUrl";
 
-                $sentencia = $pConexion->prepare($url);
-                $sentencia -> bindParam(':pUrl',$pUrl,PDO::PARAM_STR);
-                $sentencia -> execute();
+                $sentencia = $pConexion->prepare($sql);
+                $sentencia->bindParam(':pUrl', $pUrl, PDO::PARAM_STR);
+                $sentencia->execute();
 
-                $resultado = $sentencia-> fetch();
+                $resultado = $sentencia->fetch();
 
-                if($resultado['total']){
+                if ($resultado['total']) {
                     $urlExiste = true;
-
-                }
-                else{
+                } else {
                     $urlExiste = false;
                 }
-
-
-            }catch(PDOException $e){
-                print "ERROR ".$e->getMessage();
+            } catch (PDOException $e) {
+                print "ERROR " . $e->getMessage();
             }
         }
 
         return $urlExiste;
+    }
+
+    public static function borrarEntradaYComentarios($pConexion, $pIdEntrada)
+    {
+        if (isset($pConexion)) {
+            try {
+                //beginTransaction() y commit() sirve para indicar que todas las operaciones entre este bloque
+                //se tienen que hacer, se guardan en un registro secundario en el server para asegurarse de que asi
+                //suceda.
+                $pConexion -> beginTransaction();
+
+                $sql1 = "DELETE FROM comentarios WHERE entrada_id = :idEntrada";
+                $sentencia = $pConexion -> prepare($sql1);
+                $sentencia -> bindParam(':idEntrada',$pIdEntrada,PDO::PARAM_STR);
+                $sentencia -> execute();
+
+                $sql2 = "DELETE FROM entradas WHERE id = :idEntrada";
+                $sentencia = $pConexion -> prepare($sql2);
+                $sentencia -> bindParam(':idEntrada',$pIdEntrada,PDO::PARAM_STR);
+                $sentencia -> execute();
+                
+                $pConexion ->commit();
+            } catch (PDOException $e) {
+                print 'ERROR: ' . $e->getMessage();
+                //Si hubo algun error con la transaccion todo regresa a su estado anterior.
+                $pConexion -> rollBack();
+            }
+        }
     }
 }
--- vistas/borrar-entrada.php
+++ vistas/borrar-entrada.php
@@ -1,5 +1,19 @@
 <?php
+    include_once './app/config.inc.php';
+    include_once './app/CRepositorioEntrada.inc.php';
+    include_once './app/Redireccion.inc.php';
+    include_once './app/Conexion.inc.php';
+
     if(isset($_POST['borrar-entrada'])){
-        echo $_POST['id-borrar'];
+        $idEntrada = $_POST['id-borrar'];
+
+        Conexion::openConexion();
+        
+        CRepositorioEntrada::borrarEntradaYComentarios(Conexion::getConexion(),$idEntrada);
+
+        Conexion::getConexion();
+
+        Redireccion::Redirigir(RUTA_GESTOR_ENTRADAS);
+
     }
 ?>
